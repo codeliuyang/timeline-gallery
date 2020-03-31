516461cbbefc781b5589b254617666c7d3762fb3
liuyang <liuyang@yitongmed.com>
2020-03-31 17:36:51 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# timeline-gallery
-timeline gallery
+# Timeline-Gallery
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 使用 scss
+```
+npm install sass-loader node-sass --save-dev
+```
+
+```
+<style scoped lang="scss">
+      xxxx
+      xxxx
+</style>
+```
+
+### modal
+https://github.com/euvl/vue-js-modal
+```
+npm install vue-js-modal --save
+```
+
+### demo url
+https://codepen.io/brady_wright/pen/NNOvrW
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <TimelineBoard msg="Welcome to Your Vue.js App" />
+    <modal name="hello-image" @before-open="beforeOpen" height="auto" draggable=".modal-image" :resizable="true">
+      <img class="modal-image" :src="imgUrl" />
+    </modal>
+  </div>
+</template>
+
+<script>
+import TimelineBoard from './components/TimelineBoard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TimelineBoard
+  },
+  data() {
+    return {
+      imgUrl: ''
+    }
+  },
+  methods: {
+    beforeOpen(event) {
+      console.log(event.params.imgUrl)
+      this.imgUrl = event.params.imgUrl
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 10% 0 10%;
+}
+
+.modal-image {
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+</style>
